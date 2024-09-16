136d39dce1888e4e15b8133628d71570e50027d7
jakfarshodiq230 <jakfarshodiq230@gmail.com>
2024-09-16 22:04:59 +0700

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -97,6 +97,7 @@
                                         </div>
                                         
                                     </form>
+                                    <p class="text-center mt-4 mb-0">VERSI 2.1</p>
                                 </div>
                             </div>
                         </div>
